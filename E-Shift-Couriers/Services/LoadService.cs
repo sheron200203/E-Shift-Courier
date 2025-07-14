8ce3bad6b714b39cf7de73e20a0cdf5c8d890765
sheron200203 <sheron200203@gmail.com>
2025-07-14 23:21:53 +0530

    removed redundant

--- E-Shift-Couriers/Services/LoadService.cs
+++ E-Shift-Couriers/Services/LoadService.cs
@@ -23,60 +23,7 @@ namespace E_Shift_Couriers.Services
         {
             var userId = Session.CurrentUser.Id;
             var isAdmin = Session.CurrentUser.Role == UserRole.Admin;
-            return repo.GetAll(userId,isAdmin);
-        }
-
-
-        public List<LoadView> GetAll(int userId, bool isAdmin)
-        {
-            var loads = new List<LoadView>();
-            var conn = DbConnection.GetConnection();
-
-            string query = @"
-        SELECT 
-            l.LoadId,
-            l.JobId,
-            p.Name AS ProductName,
-            l.Quantity,
-            u.LorryNumber,
-            u.DriverName,
-            u.AssistantName
-        FROM Loads l
-        JOIN Products p ON l.ProductId = p.ProductId
-        LEFT JOIN TransportUnits u ON l.TransportUnitId = u.UnitId
-        JOIN Jobs j ON l.JobId = j.JobId
-    ";
-
-            if (!isAdmin)
-            {
-                query += " WHERE j.CustomerId = @userId";
-            }
-
-            var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, conn);
-            if (!isAdmin)
-            {
-                cmd.Parameters.AddWithValue("@userId", userId);
-            }
-
-            var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                loads.Add(new LoadView
-                {
-                    LoadId = Convert.ToInt32(reader["LoadId"]),
-                    JobId = Convert.ToInt32(reader["JobId"]),
-                    ProductName = reader["ProductName"].ToString(),
-                    Quantity = Convert.ToInt32(reader["Quantity"]),
-                    LorryNumber = reader["LorryNumber"].ToString(),
-                    DriverName = reader["DriverName"].ToString(),
-                    AssistantName = reader["AssistantName"].ToString()
-                });
-            }
-
-            reader.Close();
-            conn.Close();
-
-            return loads;
+            return repo.GetAll(userId, isAdmin);
         }
 
     }
